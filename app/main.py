4b403773d031f0178427622fafb4073fbc04f4e5
Viraj Ved Shankar <142313889+VirajShankar@users.noreply.github.com>
2026-06-29 12:57:37 +0530

    feat: add app/main.py for KAN-16

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
-from app.routes import appointments
+from routes.appointments import router
+from db_client.appointments import AppointmentDbClient
+from services.appointments import AppointmentService
 
-app = FastAPI(title="REST API Gateway")
+app = FastAPI()
 
-app.include_router(appointments.router)
+db_client = AppointmentDbClient()
+appointment_service = AppointmentService(db_client)
+
+
+def get_appointment_service():
+    return appointment_service
+
+
+app.include_router(router)
